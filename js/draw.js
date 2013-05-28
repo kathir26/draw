e483a4fb59f6bfaf421ecb674b68dc0373c9724a
Kathiravan <kathir26cse@gmail.com>
2013-05-28 13:00:50 +0530

    COMMIT

--- js/draw.js
+++ js/draw.js
@@ -70,30 +70,5 @@ window.onload = function() {
 }
 function color_picker()
 {
-	$('#color_pallete').ColorPicker({
-		flat: true,
-		color: '#00ff00',
-		onChange: function(hsb, hex, rgb) {
-			$('#colorSelector2 div').css('backgroundColor', '#' + hex);
-			$('#color').val('#' + hex);
-		},
-		onSubmit: function(hsb, hex, rgb) {
-			$('#colorSelector2 div').css('backgroundColor', '#' + hex);
-			$('#colorSelector2').click();
-		}
-	});
-	$('#color_pallete>div').css('position', 'absolute');
-	var widt = false;
-	$('#colorSelector2').click(function(e) {
-		$('#color_pallete').stop().animate({height: widt ? 0 : 173}, 500);
-		widt = !widt;
-		e.stopPropagation();
-	});
-	$('.colorpicker').click(function(e) {
-		e.stopPropagation();
-	});
-	$("body").click(function() {
-	  $('#color_pallete').stop().animate({height: 0}, 500);
-	  widt = false;
-	});
+	alert('=========>'+$('#color_picker').val());
 }
